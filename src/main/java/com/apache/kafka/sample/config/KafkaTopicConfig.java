bd34cc01d77ac13a791006a6b3c71c5e524d8ab4
Prashant33-py <pvasala786@gmail.com>
2025-12-10 20:42:03 +0530

    Changes yaml config to java config in Producer

--- src/main/java/com/apache/kafka/sample/config/KafkaTopicConfig.java
+++ src/main/java/com/apache/kafka/sample/config/KafkaTopicConfig.java
@@ -1,10 +1,20 @@
 package com.apache.kafka.sample.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaTopicConfig {
@@ -19,5 +29,22 @@ public class KafkaTopicConfig {
                 .partitions(4)
                 .replicas(1)
                 .build();
+    }
+
+    public Map<String, Object> producerConfig(){
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return props;
+    }
+
+    public ProducerFactory<String, Object> producerFactory(){
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
     }
 }
